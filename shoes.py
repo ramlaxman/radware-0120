bc20d7f3deaf916c0db8cb518a6bc0932a2cb6ef
Reuven M. Lerner <reuven@lerner.co.il>
2020-01-12 14:41:11 +0200

    gitautosync update 108

--- shoes.py
+++ shoes.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 [
     {'brand': 'Adidas',
      'color': 'orange',
@@ -11,8 +13,8 @@
 
 ]
 
-[one_line.split(':')[0]
- for one_line in open('/etc/passwd')
- if not one_line.startswith("#")]
+# [one_line.split(':')[0]
+#  for one_line in open('/etc/passwd')
+#  if not one_line.startswith("#")]
 
 # files.lerner.co.il --> advanced files link
