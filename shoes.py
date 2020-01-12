9c7162bb67707087a3d2c6ed52c33359cbc28ffe
Reuven M. Lerner <reuven@lerner.co.il>
2020-01-12 14:41:25 +0200

    gitautosync update 109

--- shoes.py
+++ shoes.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
 
-[
-    {'brand': 'Adidas',
-     'color': 'orange',
-     'size': '43'},
+filename = 'shoe-data.txt'
 
-    {'brand': 'Nike',
-     'color': 'black',
-     'size': '41'},
+shoes = [one_line
+         for one_line in open(filename)]
 
 
+# [
+#     {'brand': 'Adidas',
+#      'color': 'orange',
+#      'size': '43'},
 
-]
+#     {'brand': 'Nike',
+#      'color': 'black',
+#      'size': '41'},
+
+
+# ]
 
 # [one_line.split(':')[0]
 #  for one_line in open('/etc/passwd')
