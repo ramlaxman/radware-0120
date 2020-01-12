12efc7cac4b1ea5d9e69ece77c7ef8650c381187
Reuven M. Lerner <reuven@lerner.co.il>
2020-01-12 10:39:28 +0200

    gitautosync update 44

---
+++ makepw.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import random
+
+
+make_text_pw = make_password_generator('abcde')
+make_stronger_pw = make_password_generator('abcde!@#$%')
+
+short_bad_pw = make_text_pw(5)
+long_better_pw = make_stronger_pw(20)
+
+print(short_bad_pw)
+print(long_better_pw)
