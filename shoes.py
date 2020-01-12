3f12ac23998fb26e7e541f6d5910e2a7db655394
Reuven M. Lerner <reuven@lerner.co.il>
2020-01-12 16:45:23 +0200

    gitautosync update 172

--- shoes.py
+++ shoes.py
@@ -36,7 +36,7 @@ shoes = sorted(shoes, key=by_brand_and_size)
 
 sort_fields = input("Sort by: ").split()
 
-sorted(shoes, key=operator.itemgetter(*sort_fields))
+shoes = sorted(shoes, key=operator.itemgetter(*sort_fields))
 
 # (2) sort shoes first by brand, then by size within brand
 
