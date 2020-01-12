50fd48b4861ca57ca568c1da85148f1ab8a49f35
Reuven M. Lerner <reuven@lerner.co.il>
2020-01-12 12:16:44 +0200

    gitautosync update 86

--- mysum.py
+++ mysum.py
@@ -14,3 +14,12 @@ print(mysum([10, 20, 30]))
 print(mysum([10, 20.3, 30]))
 print(mysum((10, 20, 30, 40)))
 # print(mysum([10, '20', 30]))
+
+
+# [master ✗ ~/Consulting/Courses/Python/radware-0120]$ mypy --strict mysum.py
+# mysum.py:15: error: Argument 1 to "mysum" has incompatible type "Tuple[int, int, int, int]"; expected "List[Union[int, float]]"
+# mysum.py:16: error: List item 1 has incompatible type "str"; expected "float"
+# Found 2 errors in 1 file (checked 1 source file)
+# [master ✔ ~/Consulting/Courses/Python/radware-0120]$ mypy --strict mysum.py
+# mysum.py:16: error: List item 1 has incompatible type "str"; expected "float"
+# Found 1 error in 1 file (checked 1 source file)
