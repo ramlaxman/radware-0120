32685ec6e386e831a4a8d0c65e83da54f6f979c8
Reuven M. Lerner <reuven@lerner.co.il>
2020-01-19 12:24:28 +0200

    gitautosync update 118

--- emp.py
+++ emp.py
@@ -15,3 +15,20 @@ p2 = Person('name2')
 
 print(p1.greet())
 print(p2.greet())
+
+
+class Employee():
+
+    def __init__(self, name, id_number):
+        self.name = name
+        self.id_number = id_number
+
+    def greet(self):
+        return f'Hello, {self.name}'
+
+
+e1 = Employee('emp1', 1)
+e2 = Employee('emp2', 2)
+
+print(e1.greet())
+print(e2.greet())
