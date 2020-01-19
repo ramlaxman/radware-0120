f216a8cc51a34fa0433bed9e13dfdcd46c63e9bb
Reuven M. Lerner <reuven@lerner.co.il>
2020-01-19 11:53:57 +0200

    gitautosync update 93

--- person.py
+++ person.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
 
+population = 0
+
+
 class Person():
     def __init__(self, name):
         self.name = name
+        population += 1
 
     def greet(self):
         return f'Hello, {self.name}'
 
 
+print(f'Before, population = {population}')
 p1 = Person('name1')
 p2 = Person('name2')
+print(f'After, population = {population}')
 
 print(p1.greet())
 print(p2.greet())
