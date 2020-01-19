c9d08637bd4cca5540fe8ca2e293075f4e17b6c7
Reuven M. Lerner <reuven@lerner.co.il>
2020-01-19 11:52:11 +0200

    gitautosync update 89

--- person.py
+++ person.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+
+
+class Person():
+    def __init__(self, name):
+        self.name = name
+
+    def greet(self):
+        return f'Hello, {self.name}'
+
+
+p1 = Person('name1')
+p2 = Person('name2')
