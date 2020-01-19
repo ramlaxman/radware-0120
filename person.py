28c2205ae6a188b77a33f57e7a902ed479662fd0
Reuven M. Lerner <reuven@lerner.co.il>
2020-01-19 12:08:05 +0200

    gitautosync update 106

--- person.py
+++ person.py
@@ -16,6 +16,8 @@ print(f'Before, population = {Person.population}')
 p1 = Person('name1')
 p2 = Person('name2')
 print(f'After, population = {Person.population}')
+print(f'After, p1.population = {p1.population}')
+print(f'After, p2.population = {p2.population}')
 
 print(p1.greet())
 print(p2.greet())
