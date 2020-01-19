3e078621829a471ae22a8f133285c10d7e5d4039
Reuven M. Lerner <reuven@lerner.co.il>
2020-01-19 11:58:59 +0200

    gitautosync update 100

--- quiz.py
+++ quiz.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
 
 
+print("A")
+
+
 class Person():
+    print("B")
+
     def __init__(self, name):
+        print("C")
         self.name = name
 
     def greet(self):
         return f'Hello, {self.name}'
 
+    print("D")
+
+
+print("E")
 
 p1 = Person('name1')
 p2 = Person('name2')
